95c81fe7a921396780e750992991c5ae2bf324ea
Bradley Dice <bdice@bradleydice.com>
2026-08-19 00:48:17 -0500

    Update cpp/src/groupby/streaming_groupby/common.cuh
    Co-authored-by: Nghia Truong <7416935+ttnghia@users.noreply.github.com>

--- cpp/src/groupby/streaming_groupby/common.cuh
+++ cpp/src/groupby/streaming_groupby/common.cuh
@@ -239,8 +239,7 @@ auto build_cross_comparators(
     h_eqs.push_back(adapter.comparator);
   }
 
-  auto result = cudf::detail::make_device_uvector_async(h_eqs, stream, temp_mr);
-  stream.sync();
+  return cudf::detail::make_device_uvector(h_eqs, stream, temp_mr);
   return result;
 }
 
